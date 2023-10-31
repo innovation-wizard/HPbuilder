7b3ea9fb08a86422803bddc4fdde5e37e508edbb
Eya Taisei <105060182+Cyest712@users.noreply.github.com>
2023-10-31 19:04:50 +0900

    phpへのinclude、footerの追加、include時の描画のされ方の修正

---
+++ homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" />
+    <title>HPbuilder</title>
+</head>
+
+<?php
+include('header.html');
+include('toppage.html');
+include('about.html');
+include('news.html');
+include('greeting.html');
+include('suppliers.html');
+include('contact.html');
+include('company.html');
+include('footer.html');
+?>
+
+</html>
+
